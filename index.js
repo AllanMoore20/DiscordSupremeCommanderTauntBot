0d101736ffd51106405525fa3263a68f13b84a49
allan.moore20 <allan.moore20@gmail.com>
2020-08-18 18:38:31 +0100

    Add ability to play sounds, requires numbered mp3 files placed in ./sound

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 let jsonData = require('./settings.json');
 
-bot.login(jsonData.token);
+bot.login(jsonData.token_test);
 
 bot.on('ready', () =>{
     console.log('This bot is online');
@@ -10,14 +10,43 @@ bot.on('ready', () =>{
 })
 
 bot.on ('message', msg => {
+        bot.isReady = false;
         if(msg.content.startsWith(jsonData.prefix)){
-            x = msg.content.substr(jsonData.prefix.length);
-            if(jsonData.taunts.hasOwnProperty(x))
+            if(jsonData.sound)
             {
-                msg.reply(jsonData.taunts[x])
-            }else
+                playMusic(msg);
+            }
+            else
             {
-                msg.reply(jsonData.taunts['def'])
+                replyMsg(msg);
             }
         }
+        bot.isReady = true;
 })
+
+function playMusic(msg){
+    var voiceChannel = msg.member.voice.channel;
+    x = msg.content.substr(jsonData.prefix.length);
+    if(jsonData.taunts.hasOwnProperty(x) && voiceChannel)
+    {
+        voiceChannel.join().then( connection =>{
+            const dispatcher  = connection.play('./sound/'+ x + '.mp3');
+            dispatcher.on('finish', end => voiceChannel.leave());
+        })
+        .catch(console.error);        
+    }else
+    {
+        msg.reply(jsonData.taunts['def'])
+    }
+};
+
+function replyMsg(msg){
+    x = msg.content.substr(jsonData.prefix.length);
+    if(jsonData.taunts.hasOwnProperty(x))
+    {
+        msg.reply(jsonData.taunts[x])
+    }else
+    {
+        msg.reply(jsonData.taunts['def'])
+    }
+};
